b2167ac081fe0a382b07ff5f3b62c30be60bb9f1
Vikas5686 <91067260+Vikas5686@users.noreply.github.com>
2022-01-21 01:10:53 -0500

    this is finally done

---
+++ index.js
@@ -0,0 +1,7 @@
+const home = fs.readFileSync('./index.html')
+const services = fs.readFileSync('./services.html')
+const about = fs.readFileSync('./about.html')
+const cantact = fs.readFileSync('./cantact.html')
+
+const hostname = '127.0.0.1';
+const port = 3000;
